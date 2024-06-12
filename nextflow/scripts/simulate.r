e1c2ab264a387c4ddc1d80f8c7f0cda397b95231
Guppy <30294975+glstott@users.noreply.github.com>
2024-06-12 14:52:20 -0400

    Error handling for seed=8 which led to an invalid tree

--- nextflow/scripts/simulate.r
+++ nextflow/scripts/simulate.r
@@ -17,7 +17,7 @@ if (length(args) > 4) {
 # outtree <- "eg20240529.nex"
 # outcsv <- "noice.csv"
 # n <- 100
-# seed<- 12
+# seed<- 8
 
 #instantiate variables
 lambda             <- 9
@@ -32,53 +32,63 @@ low    <- function(){runif(1, 0.1, 0.3)}
 med    <- function(){runif(1, 0.3, 0.6)}
 high   <- function(){runif(1, 0.6, 0.9)}
 
-# Generate rates for pairwise combos of continents: NA-1,SA-2, E-3, AS-4,AF-5
-#   For now, we'll use the same rates for both directions of transition
-#   12 13 14 15
-rateNA_SA <- base_trans_freq * high()
-rateNA_EU <- base_trans_freq * low()
-rateNA_AS <- base_trans_freq * med()
-rateNA_AF <- base_trans_freq * low()
-#   23 24 25
-rateSA_EU <- base_trans_freq * high()
-rateSA_AS <- base_trans_freq * high()
-rateSA_AF <- base_trans_freq * med()
-#   34 35
-rateEU_AS <- base_trans_freq * med()
-rateEU_AF <- base_trans_freq * high()
-#   45
-rateAS_AF <- base_trans_freq * high()
 
-# print each rate to the console
-cat("rateNA_SA: ", rateNA_SA, "\n", "rateNA_EU: ", rateNA_EU, "\n",
-    "rateNA_AS: ", rateNA_AS, "\n", "rateNA_AF: ", rateNA_AF, "\n",
-    "rateSA_EU: ", rateSA_EU, "\n", "rateSA_AS: ", rateSA_AS, "\n",
-    "rateSA_AF: ", rateSA_AF, "\n", "rateEU_AS: ", rateEU_AS, "\n",
-    "rateEU_AF: ", rateEU_AF, "\n", "rateAS_AF: ", rateAS_AF, "\n")
-
-# Place rates along with other necessary variables into rate matrix
-p   <- c(
-  lambda, lambda, lambda, lambda, lambda,
-  mu, mu, mu, mu, mu,
-  # 12 13 14 15
-  rateNA_SA, rateNA_EU, rateNA_AS, rateNA_AF,
-  # 21 23 24 25
-  rateNA_SA, rateSA_EU, rateSA_AS, rateSA_AF,
-  # 31 32 34 35
-  rateNA_EU, rateSA_EU, rateEU_AS, rateEU_AF,
-  # 41 42 43 45
-  rateNA_AS, rateSA_AS, rateEU_AS, rateAS_AF,
-  # 51 52 53 54
-  rateNA_AF, rateSA_AF, rateEU_AF, rateAS_AF
-)
-
-print("Rate has been matrixed...")
-print(n)
-print(p)
-# generate tree and estimate discrete history
-phy <- tree.musse(p, max.taxa = n, x0=1, max.t=Inf, include.extinct=TRUE)
-print("The MUSSE has run.")
-h <- history.from.sim.discrete(phy, 1:5)
+# we add a try catch because not all discrete trait trees are guaranteed to have all discrete traits
+validGate <- F
+trycount <- 0
+while (validGate == F && trycount < 10) {
+  trycount <- trycount+1
+  tryCatch({
+    # Generate rates for pairwise combos of continents: NA-1,SA-2, E-3, AS-4,AF-5
+    #   For now, we'll use the same rates for both directions of transition
+    #   12 13 14 15
+    rateNA_SA <- base_trans_freq * high()
+    rateNA_EU <- base_trans_freq * low()
+    rateNA_AS <- base_trans_freq * med()
+    rateNA_AF <- base_trans_freq * low()
+    #   23 24 25
+    rateSA_EU <- base_trans_freq * high()
+    rateSA_AS <- base_trans_freq * high()
+    rateSA_AF <- base_trans_freq * med()
+    #   34 35
+    rateEU_AS <- base_trans_freq * med()
+    rateEU_AF <- base_trans_freq * high()
+    #   45
+    rateAS_AF <- base_trans_freq * high()
+    
+    # print each rate to the console
+    cat("rateNA_SA: ", rateNA_SA, "\n", "rateNA_EU: ", rateNA_EU, "\n",
+        "rateNA_AS: ", rateNA_AS, "\n", "rateNA_AF: ", rateNA_AF, "\n",
+        "rateSA_EU: ", rateSA_EU, "\n", "rateSA_AS: ", rateSA_AS, "\n",
+        "rateSA_AF: ", rateSA_AF, "\n", "rateEU_AS: ", rateEU_AS, "\n",
+        "rateEU_AF: ", rateEU_AF, "\n", "rateAS_AF: ", rateAS_AF, "\n")
+    
+    # Place rates along with other necessary variables into rate matrix
+    p   <- c(
+      lambda, lambda, lambda, lambda, lambda,
+      mu, mu, mu, mu, mu,
+      # 12 13 14 15
+      rateNA_SA, rateNA_EU, rateNA_AS, rateNA_AF,
+      # 21 23 24 25
+      rateNA_SA, rateSA_EU, rateSA_AS, rateSA_AF,
+      # 31 32 34 35
+      rateNA_EU, rateSA_EU, rateEU_AS, rateEU_AF,
+      # 41 42 43 45
+      rateNA_AS, rateSA_AS, rateEU_AS, rateAS_AF,
+      # 51 52 53 54
+      rateNA_AF, rateSA_AF, rateEU_AF, rateAS_AF
+    )
+    
+    print("Rate has been matrixed...")
+    print(n)
+    print(p)
+    # generate tree and estimate discrete history
+    phy <- tree.musse(p, max.taxa = n, x0=1, max.t=Inf, include.extinct=TRUE)
+    print("The MUSSE has run.")
+    h <- history.from.sim.discrete(phy, 1:5)
+    validGate <- T
+  }, error=function(e){}, finally={})
+}
 
 # a whole bunch of funny business to get things into the right format for BEAST NEXUS
 #    relabel and zap for new labels with split.
